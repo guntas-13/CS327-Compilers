3f6a412ad6a88b1d6169e0578d0b4b8dcca3b257
guntas-13 <guntassingh.saran@iitgn.ac.in>
2025-02-19 18:24:25 +0530

    restored python3.11

--- JSON/run.sh
+++ JSON/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 main.py $2 $1
+python3.11 main.py $2 $1
 
 # on other machines they might need to change this to python3 or python 
 # depending on the default python > v3.10 installed on the machine.
