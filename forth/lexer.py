ada389e2ae9da56fdf9edb9f299fb300a3f7637c
guntas-13 <guntassingh.saran@iitgn.ac.in>
2025-04-13 19:52:30 +0530

    submitted

--- forth/lexer.py
+++ forth/lexer.py
@@ -100,7 +100,7 @@ def lex(s: str) -> List[Token]:
                     print("x" * 20, "COMPILE TIME ERROR", "x" * 20, f"\nInvalid symbol character '{s[i]}' at position {i}")
                     exit(1)
             word = s[start:i]
-            if not word or not word[1].isalpha():
+            if not word or not word[1].isalpha() or word[1] != '_':
                 print("x" * 20, "COMPILE TIME ERROR", "x" * 20, f"\nInvalid symbol '{word}' at position {start}")
                 exit(1)
             tokens.append(SymbolToken(word))
