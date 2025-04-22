7196cc3b41bb874546f5c8289e2d306868167bbd
guntas-13 <guntassingh.saran@iitgn.ac.in>
2025-04-22 14:02:11 +0530

    lab exam done

--- osl/run.py
+++ osl/run.py
@@ -5,7 +5,7 @@ from codegen import *
 sys.setrecursionlimit(100000000)
 from visualizer import *
 
-with open("code.osl") as f:
+with open("q8_22110089.osl") as f:
     code = f.read()
     
 # print(code)
@@ -17,15 +17,15 @@ with open("code.osl") as f:
 parsed = parse(code)
 # pprint(parsed)
 rcode = resolve(parsed)
-pprint(rcode)
+# pprint(rcode)
 # print()
-# print(e(rcode))
+print(e(rcode))
 # print(codegen(rcode))
-bb = bytearray(codegen(rcode))
-# print(bb)
-with open("bytecode.bin", "wb") as bytecode_file:
-    bytecode_file.write(bb)
+# bb = bytearray(codegen(rcode))
+# # print(bb)
+# with open("bytecode.bin", "wb") as bytecode_file:
+#     bytecode_file.write(bb)
 
-result = parse_bytecode(bb)
-for opcode, operand in result:
-   print(f"{opcode} {operand if operand is not None else ''}")
+# result = parse_bytecode(bb)
+# for opcode, operand in result:
+#    print(f"{opcode} {operand if operand is not None else ''}")
